a2e779767682e42ca3cfa18e978568570698b878
arcsurf <ariel.couso@gmail.com>
2021-07-17 19:04:06 -0300

    Se agrega carpeta public y modificaciones en app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, './public');
-const port = 3000
+const port = 3000;
 
 app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './view/home.html'))
+    res.sendFile(path.join(__dirname, './view/home.html'));
 })
 
 app.listen(port, () => {
