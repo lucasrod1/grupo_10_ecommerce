160d072cc03d097a9504a2def02535c7937fd4bf
arcsurf <ariel.couso@gmail.com>
2021-07-23 19:03:28 -0300

    Se agrega shopping_bag.css shopping_bag.html y tambien se agrega acceso al /shopping_bag en app.js

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './view/home.html'));
 })
 
+app.get('/shopping_bag', (req, res) => {
+    res.sendFile(path.join(__dirname, './view/shopping_bag.html'));
+})
+
 app.listen(port, () => {
     console.log("OldMilano On-Line on http://127.0.0.1:" + port + " or http://localhost:"+port);
 })
