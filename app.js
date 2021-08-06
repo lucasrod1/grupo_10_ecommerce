0c800fdcd1939b63a8f5ef38e6f6c51e05d3c5dd
arcsurf <ariel.couso@gmail.com>
2021-08-06 01:27:24 -0300

    Se agrega la version 1.0 del sitio mobile

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/shopping_bag', (req, res) => {
-    res.sendFile(path.join(__dirname, './view/productCart.html.html'));
+    res.sendFile(path.join(__dirname, './view/productCart.html'));
 })
 
 app.listen(port, () => {
