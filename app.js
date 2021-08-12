33c0844a00c627a3d52811a442c415d00b620b52
Ariel Couso <ariel.couso@gmail.com>
2021-08-12 13:39:01 -0300

    Se agrega en metodo carrito la utilizacion de res.render EJS de productCart en controlador

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ const port = 3000;
 
 app.use(express.static(publicPath));
 
+// Configuracion EJS
+app.set('views', './view');
+app.set('views engine', 'ejs');
+
+// Configuracion de rutas a sitio
 app.use('/shopping_bag', routesCarrito);
 
+// Configuracion Server
 app.listen(port, () => {
     console.log("OldMilano On-Line on http://127.0.0.1:" + port + " or http://localhost:"+port);
 })
--- controllers/controllerOldMilano.js
+++ controllers/controllerOldMilano.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const controllerOldMilano = {
     carrito: function(req, res){
-        res.sendFile(path.join(__dirname, '../view/productCart.html'));
+        // res.sendFile(path.join(__dirname, '../view/productCart.html'));
+        res.render('productCart.ejs')
     },
 }
 
