d4cde78b5db6d9a3cfcd45237881fa154967309e
Ariel Couso <ariel.couso@gmail.com>
2021-08-12 00:27:31 -0300

    Punto4 Sprint3 - Se agregar controllerOldMilano.js y routes.Carrito.js tambien se agrega ruta especifica a shopgping_bag en app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const routesCarrito = require('./routes/routesCarrito')
 const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, './public');
@@ -6,13 +7,7 @@ const port = 3000;
 
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './view/home.html'));
-})
-
-app.get('/shopping_bag', (req, res) => {
-    res.sendFile(path.join(__dirname, './view/productCart.html'));
-})
+app.use('/shopping_bag', routesCarrito);
 
 app.listen(port, () => {
     console.log("OldMilano On-Line on http://127.0.0.1:" + port + " or http://localhost:"+port);
