4ece830e77c0611cfee97ab5483178a515fa307e
arcsurf <ariel.couso@gmail.com>
2021-09-16 17:27:03 -0300

    Se modifica el loginValidate para crear dentro de este varios metodos para validadcion de diferentes secciones de la pagina validando usuario

--- src/middlewares/loginValidate.js
+++ src/middlewares/loginValidate.js
@@ -1,8 +1,19 @@
-module.exports = (req, res, next) => {
+const loginValidate = {
+    cart: (req, res, next) => {
     if(req.session.user){
         next();
     }else{
         res.redirect('/users/login')
     }
+    },
+    profile: (req, res, next) => {
+        if (req.session.user) {
+            next();
+        } else {
+            res.redirect('/user/profile')
+        }
+    }
 
 }
+
+module.exports = loginValidate
--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -5,9 +5,9 @@ const loginValidate = require('../middlewares/loginValidate');
 
 
 router.get('/', mainController.home);
-router.get('/login', mainController.login);
+router.get('/login', loginValidate.profile, mainController.login);
 router.get('/register', mainController.register);
 router.get('/products/detail', mainController.productDetail);
-router.get('/cart', loginValidate, mainController.cart);
+router.get('/cart', loginValidate.cart, mainController.cart);
 
 module.exports = router;
