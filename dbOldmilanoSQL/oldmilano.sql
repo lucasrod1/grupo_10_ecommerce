081991a5fc9457a0ac12f4236c3d9afed0711049
arcsurf <ariel.couso@gmail.com>
2021-10-22 00:01:07 -0300

    Se agrega modificacion en script de creacion de base de datos, diagrama relacional

--- dbOldmilanoSQL/oldmilano.sql
+++ dbOldmilanoSQL/oldmilano.sql
@@ -30,13 +30,10 @@ CREATE TABLE IF NOT EXISTS `category` (
 -- Volcando estructura para tabla oldmilano.colorproduct
 CREATE TABLE IF NOT EXISTS `colorproduct` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_products` int(11) DEFAULT NULL,
   `id_colors` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `colorproduct_FK` (`id_products`),
-  KEY `colorproduct_FK_1` (`id_colors`),
-  CONSTRAINT `colorproduct_FK` FOREIGN KEY (`id_products`) REFERENCES `products` (`id`),
-  CONSTRAINT `colorproduct_FK_1` FOREIGN KEY (`id_colors`) REFERENCES `colors` (`id`)
+  KEY `colorproduct_FK` (`id_colors`),
+  CONSTRAINT `colorproduct_FK` FOREIGN KEY (`id_colors`) REFERENCES `colors` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Volcando datos para la tabla oldmilano.colorproduct: ~0 rows (aproximadamente)
@@ -54,31 +51,19 @@ CREATE TABLE IF NOT EXISTS `colors` (
 /*!40000 ALTER TABLE `colors` DISABLE KEYS */;
 /*!40000 ALTER TABLE `colors` ENABLE KEYS */;
 
--- Volcando estructura para tabla oldmilano.discount
-CREATE TABLE IF NOT EXISTS `discount` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Volcando datos para la tabla oldmilano.discount: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `discount` DISABLE KEYS */;
-/*!40000 ALTER TABLE `discount` ENABLE KEYS */;
-
 -- Volcando estructura para tabla oldmilano.ordersummary
 CREATE TABLE IF NOT EXISTS `ordersummary` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_user` int(11) DEFAULT NULL,
-  `id_productsOrder` int(11) DEFAULT NULL,
+  `id_productOrder` int(11) DEFAULT NULL,
   `orderAmount` float DEFAULT NULL,
-  `shippingAddress` varchar(255) DEFAULT NULL,
+  `shippingAddress` varchar(100) DEFAULT NULL,
   `orderDate` date DEFAULT NULL,
-  `price` float DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `ordersummary_FK` (`id_user`),
-  KEY `ordersummary_FK_1` (`id_productsOrder`),
+  KEY `ordersummary_FK_1` (`id_productOrder`),
   CONSTRAINT `ordersummary_FK` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
-  CONSTRAINT `ordersummary_FK_1` FOREIGN KEY (`id_productsOrder`) REFERENCES `productorder` (`id`)
+  CONSTRAINT `ordersummary_FK_1` FOREIGN KEY (`id_productOrder`) REFERENCES `productorder` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Volcando datos para la tabla oldmilano.ordersummary: ~0 rows (aproximadamente)
@@ -90,14 +75,19 @@ CREATE TABLE IF NOT EXISTS `productorder` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_users` int(11) DEFAULT NULL,
   `id_products` int(11) DEFAULT NULL,
-  `productquantity` int(11) DEFAULT NULL,
-  `pricecolumn1` float DEFAULT NULL,
-  `pricetotal` float DEFAULT NULL,
+  `productQuantity` int(11) DEFAULT NULL,
+  `priceTotal` float DEFAULT NULL,
+  `id_colorProduct` int(11) DEFAULT NULL,
+  `id_productSize` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `productorder_FK` (`id_users`),
   KEY `productorder_FK_1` (`id_products`),
+  KEY `productorder_FK_2` (`id_colorProduct`),
+  KEY `productorder_FK_3` (`id_productSize`),
   CONSTRAINT `productorder_FK` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`),
-  CONSTRAINT `productorder_FK_1` FOREIGN KEY (`id_products`) REFERENCES `products` (`id`)
+  CONSTRAINT `productorder_FK_1` FOREIGN KEY (`id_products`) REFERENCES `products` (`id`),
+  CONSTRAINT `productorder_FK_2` FOREIGN KEY (`id_colorProduct`) REFERENCES `colorproduct` (`id`),
+  CONSTRAINT `productorder_FK_3` FOREIGN KEY (`id_productSize`) REFERENCES `productsize` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Volcando datos para la tabla oldmilano.productorder: ~0 rows (aproximadamente)
@@ -109,21 +99,17 @@ CREATE TABLE IF NOT EXISTS `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `description` text DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
   `id_category` int(11) DEFAULT NULL,
   `price` float DEFAULT NULL,
-  `id_discount` int(11) DEFAULT NULL,
+  `id_colorProduct` int(11) DEFAULT NULL,
   `id_productSize` int(11) DEFAULT NULL,
-  `id_colors` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `products_FK` (`id_category`),
-  KEY `products_FK_1` (`id_discount`),
+  KEY `products_FK_1` (`id_colorProduct`),
   KEY `products_FK_2` (`id_productSize`),
-  KEY `products_FK_3` (`id_colors`),
   CONSTRAINT `products_FK` FOREIGN KEY (`id_category`) REFERENCES `category` (`id`),
-  CONSTRAINT `products_FK_1` FOREIGN KEY (`id_discount`) REFERENCES `discount` (`id`),
-  CONSTRAINT `products_FK_2` FOREIGN KEY (`id_productSize`) REFERENCES `productsize` (`id`),
-  CONSTRAINT `products_FK_3` FOREIGN KEY (`id_colors`) REFERENCES `colors` (`id`)
+  CONSTRAINT `products_FK_1` FOREIGN KEY (`id_colorProduct`) REFERENCES `colorproduct` (`id`),
+  CONSTRAINT `products_FK_2` FOREIGN KEY (`id_productSize`) REFERENCES `productsize` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Volcando datos para la tabla oldmilano.products: ~0 rows (aproximadamente)
@@ -133,13 +119,10 @@ CREATE TABLE IF NOT EXISTS `products` (
 -- Volcando estructura para tabla oldmilano.productsize
 CREATE TABLE IF NOT EXISTS `productsize` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_products` int(11) DEFAULT NULL,
   `id_sizes` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `productsize_FK` (`id_products`),
-  KEY `productsize_FK_1` (`id_sizes`),
-  CONSTRAINT `productsize_FK` FOREIGN KEY (`id_products`) REFERENCES `products` (`id`),
-  CONSTRAINT `productsize_FK_1` FOREIGN KEY (`id_sizes`) REFERENCES `sizes` (`id`)
+  KEY `productsize_FK` (`id_sizes`),
+  CONSTRAINT `productsize_FK` FOREIGN KEY (`id_sizes`) REFERENCES `sizes` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Volcando datos para la tabla oldmilano.productsize: ~0 rows (aproximadamente)
@@ -149,7 +132,7 @@ CREATE TABLE IF NOT EXISTS `productsize` (
 -- Volcando estructura para tabla oldmilano.sizes
 CREATE TABLE IF NOT EXISTS `sizes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `size` varchar(10) DEFAULT NULL,
+  `size` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -160,15 +143,15 @@ CREATE TABLE IF NOT EXISTS `sizes` (
 -- Volcando estructura para tabla oldmilano.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(100) DEFAULT NULL,
-  `lastname` varchar(100) DEFAULT NULL,
+  `firstName` varchar(100) DEFAULT NULL,
+  `lastName` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
-  `id_usertype` int(11) DEFAULT NULL,
-  `avatarimage` varchar(255) DEFAULT NULL,
+  `id_userType` int(11) DEFAULT NULL,
+  `avatarImage` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `users_FK` (`id_usertype`),
-  CONSTRAINT `users_FK` FOREIGN KEY (`id_usertype`) REFERENCES `usertype` (`id`)
+  KEY `users_FK` (`id_userType`),
+  CONSTRAINT `users_FK` FOREIGN KEY (`id_userType`) REFERENCES `usertype` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Volcando datos para la tabla oldmilano.users: ~0 rows (aproximadamente)
