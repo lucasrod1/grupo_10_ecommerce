e5a7a0513db7851cb106ad0ee01dd146cf51b40d
arcsurf <ariel.couso@gmail.com>
2021-11-24 00:13:31 -0300

    Se agrega ruta desde apiRoute a metodo de controllerApi productsId

--- src/routes/apiRoute.js
+++ src/routes/apiRoute.js
@@ -2,15 +2,10 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const controllerApi = require('../controllers/controllerApi');
-const fs = require('fs');
 
 router.get('/users', controllerApi.users);
 router.get('/users/:id', controllerApi.byUserId);
 router.get('/products', controllerApi.products);
-// router.get('/users/:id', controllerApi.byUserId);
-// router.post('/login', userValidator.login, usersController.loginValidation);
-// router.post('/logout', usersController.logout);
-// router.get('/register', usersController.register);
-// router.delete('/delete', usersController.delete)
+router.get('/products/:id', controllerApi.prouctsId);
 
 module.exports = router;
