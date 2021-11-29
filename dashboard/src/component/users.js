c25069f5c9dbbddae6c0541f537595879eb52133
arcsurf <ariel.couso@gmail.com>
2021-11-29 20:56:28 -0300

    Se esta trabajando en axios para poder resolver la query a la API

--- dashboard/src/component/users.js
+++ dashboard/src/component/users.js
@@ -1,21 +1,16 @@
-// import axios from 'axios';
+import axios from 'axios';
 import React from 'react';
 
 function User() {
-    let users = fetch('http://127.0.0.1:3001/api/users')
-    .then(response => response.json())
-    .then(data => console.log(data));
-
-
-    // let users = axios.get('https://127.0.0.1:3001/api/users')
-    // .then(response => {
-    // console.log(response);
-    // });
+    axios.get('http://127.0.0.1:3001/api/users')
+    .then(response => {
+    console.log(response);
+    });
     console.log("Imprime ok")
     return (
       <div className="UserCount">
         <h1>User Count</h1>
-        <h2>{users.userCount}</h2>
+        <h2></h2>
       </div>
     );
   }
